ca5e36ff846b8aa092934e20cb9dac70c98be935
HasithDeAlwis <hasithde24@gmail.com>
2025-05-18 19:10:21 -0400

    feat(api): scaffold users endpoint

---
+++ apps/api/src/User/Api.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+
+module User.Api where
+
+import GHC.Generics (Generic)
+import Servant (Get, JSON, (:<|>), (:>))
+import Shared.Models.User (User)
+
+type API =
+  "users" :> Get '[JSON] [User]
+    :<|> "albert" :> Get '[JSON] User
+    :<|> "issac" :> Get '[JSON] User
