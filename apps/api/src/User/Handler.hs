612b5bb60d9e2860551070449121ad3963c80d77
HasithDeAlwis <hasithde24@gmail.com>
2025-05-19 13:19:32 -0400

    chore(api): update user handler

--- apps/api/src/User/Handler.hs
+++ apps/api/src/User/Handler.hs
@@ -1,15 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module User.Handler where
 
+import Data.Maybe (fromJust)
+import Data.Time (UTCTime, defaultTimeLocale, parseTimeOrError)
+import Data.UUID (UUID, fromString)
 import Shared.Models.User (User (..))
 
-users :: [User]
-users =
-  [ User "Isaac Newton" 372 "isaac@newton.co.uk",
-    User "Albert Einstein" 136 "ae@mc2.org"
-  ]
+-- Hardcoded UUID helper
+uuid :: String -> UUID
+uuid = fromJust . fromString
+
+-- Hardcoded UTCTime helper (ISO 8601 string)
+time :: String -> UTCTime
+time = parseTimeOrError True defaultTimeLocale "%Y-%m-%dT%H:%M:%S%QZ"
 
 isaac :: User
-isaac = User "Isaac Newton" 372 "isaac@newton.co.uk"
+isaac =
+  User
+    { user_id = uuid "123e4567-e89b-12d3-a456-426614174000",
+      user_name = "Isaac Newton",
+      user_email = "isaac@newton.co.uk",
+      user_created_at = time "2020-01-01T00:00:00Z"
+    }
 
 albert :: User
-albert = User "Albert Einstein" 136 "ae@mc2.org"
+albert =
+  User
+    { user_id = uuid "123e4567-e89b-12d3-a456-426614174001",
+      user_name = "Albert Einstein",
+      user_email = "ae@mc2.org",
+      user_created_at = time "2020-01-01T00:00:00Z"
+    }
+
+users :: [User]
+users = [isaac, albert]
