da354ad62536b32ecdfa90504bf05008016d0469
HasithDeAlwis <hasithde24@gmail.com>
2025-05-20 18:27:41 -0400

    feat: add login route

---
+++ apps/sandbox/app/routes/login.tsx
@@ -0,0 +1,25 @@
+import { redirect } from 'react-router';
+import { axiosInstance } from '@sandbox/shared';
+import { LoginCard } from '@sandbox/features/login/ui';
+
+export async function clientLoader() {
+  try {
+    const res = await axiosInstance.get('/me', {
+      withCredentials: true,
+      validateStatus: () => true,
+    });
+
+    if (res.status === 200) {
+      // save to context
+      return redirect('/')
+    }
+  } catch (err) {
+    console.error('Client loader error:', err);
+  }
+}
+
+export default function Login() {
+  return (
+    <LoginCard />
+  );
+}
