d2a8a52a2c5d463e2ee9fd846922a03740d2f574
HasithDeAlwis <hasithde24@gmail.com>
2025-05-21 12:44:15 -0400

    feat(sandbox): create api models

---
+++ libs/sandbox/models/project.ts
@@ -0,0 +1,8 @@
+export interface Project {
+    project_id: string;
+    project_workspace_id: string;
+    project_name: string;
+    project_created_at: string;
+    project_updated_at: string;
+    project_is_archived: boolean;
+}
