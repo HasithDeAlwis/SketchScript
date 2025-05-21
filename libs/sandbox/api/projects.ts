4ffd59e1d51506104e687bd9e9a461c5b7800681
HasithDeAlwis <hasithde24@gmail.com>
2025-05-21 12:46:10 -0400

    feat(sandbox): on home route find all projects and files

--- libs/sandbox/api/projects.ts
+++ libs/sandbox/api/projects.ts
@@ -1,6 +1,37 @@
-import { axiosInstance } from '@sandbox/shared'
+import { axiosInstance } from '../shared/axiosInstance';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { Project } from '../models/project';
 
-export async function getFilesByProjectID(pid: string) {
-  const response = await axiosInstance.get(`/project?wid=${pid}`)
-  return response.data;
+export async function fetchProjectsByWorkspaceId(workspaceId: string): Promise<Project[]> {
+  const res = await axiosInstance.get<Project[]>(`/project?wid=${workspaceId}`, {
+    withCredentials: true,
+    validateStatus: () => true,
+  });
+
+  if (res.status !== 200) {
+    throw new Error(`project?wid=${workspaceId}`);
+  }
+
+  return res.data;
 }
+
+
+export const projectApi = createApi({
+  reducerPath: 'projectApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_PUBLIC_API_BASE,
+    credentials: 'include',
+  }),
+  tagTypes: ['Project'],
+  endpoints: (builder) => ({
+    updateProject: builder.mutation<void, Project>({
+      query: (project) => ({
+        url: `project/${project.project_id}`,
+        method: 'PUT',
+        body: { name: project.project_name },
+      }),
+    }),
+  }),
+});
+
+export const { useUpdateProjectMutation } = projectApi;
