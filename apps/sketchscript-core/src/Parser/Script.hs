9e6b3e52df2bc07163abebb039f07574c5614f36
HasithDeAlwis <hasithde24@gmail.com>
2025-06-01 19:32:26 -0400

    feat(core): implement parseSketchScript to take string and directlty parse

--- apps/sketchscript-core/src/Parser/Script.hs
+++ apps/sketchscript-core/src/Parser/Script.hs
@@ -1,7 +1,8 @@
-module Parser.Script (parseScript) where
+module Parser.Script (parseSketchScript) where
 
 import Parser.Element (parseElements)
 import SketchScriptTypes (Element)
+import Text.Megaparsec (errorBundlePretty, parse)
 import qualified Text.Megaparsec.Char as TMC
 import Types (Parser)
 
@@ -9,3 +10,9 @@ parseScript :: Parser [Element]
 parseScript = do
   TMC.space
   parseElements 1
+
+parseSketchScript :: String -> Either String [Element]
+parseSketchScript input =
+  case parse parseScript "sketch-script" input of
+    Left err -> Left (errorBundlePretty err)
+    Right ast -> Right ast
