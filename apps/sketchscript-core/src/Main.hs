a602ef6b9ba3baf2695a3b2cddc0a6888c17a006
HasithDeAlwis <hasithde24@gmail.com>
2025-06-01 19:32:26 -0400

    feat(core): implement parseSketchScript to take string and directlty parse

--- apps/sketchscript-core/src/Main.hs
+++ apps/sketchscript-core/src/Main.hs
@@ -2,15 +2,10 @@ module Main where
 
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
-import Data.Void (Void)
-import Parser.Script (parseScript)
-import Text.Megaparsec
-import Types (Parser)
+import Parser.Script (parseSketchScript)
 
 main :: IO ()
 main = do
   putStrLn "Enter your script:"
   input <- TIO.getContents
-  case parse parseScript "<stdin>" (T.unpack input) of
-    Left err -> putStrLn (errorBundlePretty err)
-    Right ast -> print ast
+  print $ parseSketchScript (T.unpack input)
